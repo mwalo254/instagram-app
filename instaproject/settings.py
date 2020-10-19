416c2d2c520dc2e7406e199db946f7908c16db9b
mwalo254 <mwalonick@gmail.com>
2020-10-19 23:28:44 +0300

    all change

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -116,8 +116,8 @@ WSGI_APPLICATION = 'instaproject.wsgi.application'
  #   'default': {
   #      'ENGINE': 'django.db.backends.sqlite3',
    #     'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+   # }
+#}
 
 
 # Password validation
